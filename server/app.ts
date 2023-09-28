94390643ac07ee9811cc8d337a2bb0e4b706d7d6
Peter Phillips <peter@greenthistle.co.uk>
2023-09-28 09:54:14 +0100

    SDIT-1108: ✨ Write environment name in nunjucks setup instead

--- server/app.ts
+++ server/app.ts
@@ -18,7 +18,6 @@ import setUpWebSession from './middleware/setUpWebSession'
 
 import routes from './routes'
 import type { Services } from './services'
-import setUpEnvironmentName from './middleware/setUpEnvironmentName'
 
 export default function createApp(services: Services): express.Application {
   const app = express()
@@ -33,7 +32,6 @@ export default function createApp(services: Services): express.Application {
   app.use(setUpWebSession())
   app.use(setUpWebRequestParsing())
   app.use(setUpStaticResources())
-  setUpEnvironmentName(app)
   nunjucksSetup(app, services.applicationInfo)
   app.use(setUpAuthentication())
   app.use(authorisationMiddleware())
--- server/middleware/setUpEnvironmentName.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-param-reassign */
-import express from 'express'
-import config from '../config'
-
-export default (app: express.Express) => {
-  app.locals.environmentName = config.environmentName
-  app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
-}
