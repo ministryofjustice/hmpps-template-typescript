9d3480a45253781012776ad4be4457120e051dbe
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2024-06-11 15:47:57 +0100

    MAP-1255 Add accessibility testing to integration tests by default

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 import { resetStubs } from './integration_tests/mockApis/wiremock'
 import auth from './integration_tests/mockApis/auth'
 import tokenVerification from './integration_tests/mockApis/tokenVerification'
+import logAccessibilityViolations from './integration_tests/support/accessibilityViolations'
 
 export default defineConfig({
   chromeWebSecurity: false,
@@ -19,6 +20,7 @@ export default defineConfig({
         reset: resetStubs,
         ...auth,
         ...tokenVerification,
+        ...logAccessibilityViolations,
       })
     },
     baseUrl: 'http://localhost:3007',
--- integration_tests/pages/page.ts
+++ integration_tests/pages/page.ts
@@ -1,3 +1,6 @@
+import 'cypress-axe'
+import { Result } from 'axe-core'
+
 export type PageElement = Cypress.Chainable<JQuery>
 
 export default abstract class Page {
@@ -5,15 +8,46 @@ export default abstract class Page {
     return new constructor()
   }
 
-  constructor(private readonly title: string) {
-    this.checkOnPage()
+  constructor(
+    private readonly title: string,
+    private readonly skipA11y = false,
+  ) {
+    if (title) {
+      this.checkOnPage()
+    }
   }
 
   checkOnPage(): void {
     cy.get('h1').contains(this.title)
+    if (!(Cypress.env('SKIP_AXE') || this.skipA11y)) {
+      cy.injectAxe()
+      cy.configureAxe({
+        rules: [
+          // Temporary rule whilst this issue is resolved https://github.com/w3c/aria/issues/1404
+          { id: 'aria-allowed-attr', reviewOnFail: true },
+          // Ignore the "All page content should be contained by landmarks", which conflicts with GOV.UK guidance (https://design-system.service.gov.uk/components/back-link/#how-it-works)
+          { id: 'region', reviewOnFail: true, selector: '.govuk-back-link' },
+        ],
+      })
+      cy.checkA11y(undefined, undefined, this.logAccessibilityViolations)
+    }
   }
 
   signOut = (): PageElement => cy.get('[data-qa=signOut]')
 
   manageDetails = (): PageElement => cy.get('[data-qa=manageDetails]')
+
+  logAccessibilityViolations(violations: Result[]): void {
+    cy.task('logAccessibilityViolationsSummary', `Accessibility violations detected: ${violations.length}`)
+
+    const violationData = violations.map(({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length,
+      nodeTargets: nodes.map(node => node.target).join(' - '),
+    }))
+
+    cy.task('logAccessibilityViolationsTable', violationData)
+  }
 }
