861950136f445fa12d65b3df9955e6d51adce951
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-06-12 16:28:08 +0100

    SDIT-1802: ✨ Add request session to tests (#374)

--- server/routes/testutils/appSetup.ts
+++ server/routes/testutils/appSetup.ts
@@ -10,6 +10,7 @@ import type { Services } from '../../services'
 import type { ApplicationInfo } from '../../applicationInfo'
 import AuditService from '../../services/auditService'
 import { HmppsUser } from '../../interfaces/hmppsUser'
+import setUpWebSession from '../../middleware/setUpWebSession'
 
 jest.mock('../../services/auditService')
 
@@ -40,6 +41,7 @@ function appSetup(services: Services, production: boolean, userSupplier: () => H
   app.set('view engine', 'njk')
 
   nunjucksSetup(app, testAppInfo)
+  app.use(setUpWebSession())
   app.use((req, res, next) => {
     req.user = userSupplier() as Express.User
     req.flash = flashProvider
