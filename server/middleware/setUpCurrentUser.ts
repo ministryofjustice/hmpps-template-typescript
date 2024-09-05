28f235c9ba7b8facb708d08246edc7e4993d34a4
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-05 18:17:08 +0100

    Merge populateCurrentUser and setupCurrentUser

--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -1,8 +1,41 @@
-import { Router } from 'express'
-import populateCurrentUser from './populateCurrentUser'
+import { jwtDecode } from 'jwt-decode'
+import express from 'express'
+import { convertToTitleCase } from '../utils/utils'
+import logger from '../../logger'
 
-export default function setUpCurrentUser(): Router {
-  const router = Router({ mergeParams: true })
-  router.use(populateCurrentUser())
+export default function setUpCurrentUser() {
+  const router = express.Router()
+
+  router.use((req, res, next) => {
+    try {
+      const {
+        name,
+        user_id: userId,
+        authorities: roles = [],
+      } = jwtDecode(res.locals.user.token) as {
+        name?: string
+        user_id?: string
+        authorities?: string[]
+      }
+
+      res.locals.user = {
+        ...res.locals.user,
+        userId,
+        name,
+        displayName: convertToTitleCase(name),
+        userRoles: roles.map(role => role.substring(role.indexOf('_') + 1)),
+      }
+
+      if (res.locals.user.authSource === 'nomis') {
+        res.locals.user.staffId = parseInt(userId, 10) || undefined
+      }
+
+      next()
+    } catch (error) {
+      logger.error(error, `Failed to populate user details for: ${res.locals.user && res.locals.user.username}`)
+      next(error)
+    }
+  })
+
   return router
 }
