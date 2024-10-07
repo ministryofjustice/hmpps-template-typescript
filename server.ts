8741a5ead03d55caca8a584fea17adaa86d4c3f0
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-10-07 12:27:32 +0100

    lint error

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import 'applicationinsights'
 
 import app from './server/index'
 import logger from './logger'
-
+;
 app.listen(app.get('port'), () => {
   logger.info(`Server listening on port ${app.get('port')}`)
 })
