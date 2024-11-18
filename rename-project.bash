6440d1f5d6754d94481b85b78b2f4b1edfc15fd0
sandhya1874 <sandhya.gandalwar@digital.justice.gov.uk>
2024-11-18 09:15:41 +0000

    Removed circleci config update

--- rename-project.bash
+++ rename-project.bash
@@ -68,14 +68,6 @@ sed -i -z -E \
   -e "s/PROD_ALERTS_SEVERITY_LABEL/$PROD_ALERTS_SEVERITY_LABEL/" \
   helm_deploy/values-prod.yaml
 
-# change cron job to be random time otherwise we hit rate limiting with veracode
-RANDOM_HOUR=$((RANDOM % (9 - 3 + 1) + 3))
-RANDOM_MINUTE=$(($RANDOM%60))
-RANDOM_MINUTE2=$(($RANDOM%60))
-sed -i -z -E \
-  -e "s/SLACK_RELEASES_CHANNEL/$SLACK_RELEASES_CHANNEL/" \
-  .circleci/config.yml
-
 # lastly remove ourselves
 rm rename-project.bash
 
