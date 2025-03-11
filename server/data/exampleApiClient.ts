0da21adf58b991b87a2c1099cf36bf5402d0c278
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-03-11 12:29:30 +0000

    Update exampleApiClient and add tests

--- server/data/exampleApiClient.ts
+++ server/data/exampleApiClient.ts
@@ -1,17 +1,10 @@
-import {
-  RestClient,
-  TokenStore,
-  getSystemClientTokenFromHmppsAuth,
-  asSystem,
-} from '@ministryofjustice/hmpps-rest-client'
+import { RestClient, asSystem, AuthenticationClient } from '@ministryofjustice/hmpps-rest-client'
 import config from '../config'
 import logger from '../../logger'
 
 export default class ExampleApiClient extends RestClient {
-  constructor(tokenStore: TokenStore) {
-    super('Example API', config.apis.exampleApi, logger, (username: string) =>
-      getSystemClientTokenFromHmppsAuth(config.apis.hmppsAuth, tokenStore, logger, username),
-    )
+  constructor(authenticationClient: AuthenticationClient) {
+    super('Example API', config.apis.exampleApi, logger, authenticationClient)
   }
 
   getCurrentTime() {
