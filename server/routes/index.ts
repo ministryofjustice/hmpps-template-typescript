40ee5b3db9eade45fefabdbc19a67409ea5dab79
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-04-02 18:23:09 +0100

    Add example error handling

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,10 @@
 import { type RequestHandler, Router } from 'express'
 
+import createError from 'http-errors'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import { Page } from '../services/auditService'
+import logger from '../../logger'
 
 export default function routes({ auditService, exampleService }: Services): Router {
   const router = Router()
@@ -10,9 +12,16 @@ export default function routes({ auditService, exampleService }: Services): Rout
 
   get('/', async (req, res, next) => {
     await auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user.username, correlationId: req.id })
-    const currentTime = await exampleService.getCurrentTime()
 
-    res.render('pages/index', { currentTime })
+    try {
+      const currentTime = await exampleService.getCurrentTime()
+
+      return res.render('pages/index', { currentTime })
+    } catch (e) {
+      logger.error(e)
+
+      return next(createError(500, 'There was an issue with the exampleApi'))
+    }
   })
 
   return router
