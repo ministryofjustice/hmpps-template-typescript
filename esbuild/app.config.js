ba982b73faeb60013d81cd9df711eaef524e8024
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-10-27 19:40:26 +0000

    Tidy

--- esbuild/app.config.js
+++ esbuild/app.config.js
@@ -6,23 +6,21 @@ const { buildNotificationPlugin } = require('./utils')
 /**
  * Build typescript application into CommonJS
  */
-const getAppConfig = buildConfig => {
-  return {
-    entryPoints: globSync(buildConfig.app.entryPoints),
-    outdir: buildConfig.app.outDir,
-    bundle: false,
-    sourcemap: true,
-    platform: 'node',
-    format: 'cjs',
-    plugins: [
-      typecheckPlugin({ watch: buildConfig.isWatchMode }),
-      copy({
-        resolveFrom: 'cwd',
-        assets: buildConfig.app.copy,
-      }),
-      buildNotificationPlugin('App', buildConfig.isWatchMode),
-    ],
-  }
-}
+const getAppConfig = buildConfig => ({
+  entryPoints: globSync(buildConfig.app.entryPoints),
+  outdir: buildConfig.app.outDir,
+  bundle: false,
+  sourcemap: true,
+  platform: 'node',
+  format: 'cjs',
+  plugins: [
+    typecheckPlugin({ watch: buildConfig.isWatchMode }),
+    copy({
+      resolveFrom: 'cwd',
+      assets: buildConfig.app.copy,
+    }),
+    buildNotificationPlugin('App', buildConfig.isWatchMode),
+  ],
+})
 
 module.exports = { getAppConfig }
