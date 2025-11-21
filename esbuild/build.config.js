1c763ea148154bf3745a122d5a563e965d0edf3a
Christopher Moss <chris.moss1@justice.gov.uk>
2025-11-21 08:31:34 +0000

    Copy views + image assets conditionally on isWatchMode

--- esbuild/build.config.js
+++ esbuild/build.config.js
@@ -22,7 +22,7 @@ const getBuildConfig = () => {
         {
           from: path.join(cwd, 'server/views/**/*'),
           to: path.join(cwd, 'dist/server/views'),
-          watch: true,
+          watch: isWatchMode,
         },
       ],
     },
@@ -34,6 +34,7 @@ const getBuildConfig = () => {
         {
           from: path.join(cwd, 'assets/images/**/*'),
           to: path.join(cwd, 'dist/assets/images'),
+          watch: isWatchMode,
         },
       ],
       clear: globSync([path.join(cwd, 'dist/assets/{css,js}')]),
