40b0461fe84b66ff311c851d15cb1ca810b46e92
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2026-08-19 12:30:28 +0100

    Validate log level and add changelog (#808)

--- logger.ts
+++ logger.ts
@@ -1,12 +1,21 @@
+import assert from 'assert'
 import bunyan from 'bunyan'
 import bunyanFormat from 'bunyan-format'
 import config from './server/config'
 
+export function validateLogLevel(level: string): asserts level is bunyan.LogLevelString {
+  const validLogLevels: Array<bunyan.LogLevelString> = ['trace', 'debug', 'info', 'warn', 'error', 'fatal']
+  assert(
+    validLogLevels.includes(level as bunyan.LogLevelString),
+    `Invalid log level: '${logLevel}', valid options: '${validLogLevels}'`,
+  )
+}
+
 const formatOut = bunyanFormat({ outputMode: 'short', color: !config.production })
 
-const validLogLevels = new Set(['trace', 'debug', 'info', 'warn', 'error', 'fatal'])
-const envLogLevel = process.env.LOG_LEVEL?.toLowerCase().trim() || 'info'
-const logLevel: bunyan.LogLevel = validLogLevels.has(envLogLevel) ? (envLogLevel as bunyan.LogLevel) : 'info'
+const logLevel = process.env.LOG_LEVEL?.toLowerCase().trim() || 'info'
+
+validateLogLevel(logLevel)
 
 const logger = bunyan.createLogger({
   name: 'HMPPS Typescript Template',
